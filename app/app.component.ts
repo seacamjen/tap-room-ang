ca32afe20b67b96ff304ec8db12741440e2d8ab5
Cameron Jensen and Shane Stafford <cameron+shane@>
2017-05-03 16:09:07 -0700

    add navbar and admin view to UI

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,19 +4,44 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'app-root',
   template:`
+  <div class="container-fluid header">
+  </div>
+  <nav class="navbar navbar-default" data-spy="affix" data-offset-top="200">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">Tappity Taps</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="/">Home</a></li>
+        <li><a (click)="toggleAdmin()">Admin View</a></li>
+        <li *ngIf="adminView"><a (click)="toggleButtons()">Customer View</a></li>
+      </ul>
+    </div>
+  </nav>
   <div class="container">
-    <h1>Tappity Taps</h1>
     <keg-list [childKegList]="allKegs"></keg-list>
-    <new-keg (newKegSender)="addKeg($event)"></new-keg>
+
+    <div *ngIf="adminView">
+      <new-keg (newKegSender)="addKeg($event)"></new-keg>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
   allKegs: Keg[] = [];
+  adminView = false;
 
   addKeg(newKegFromChild: Keg) {
     this.allKegs.push(newKegFromChild);
+  }
+
+  toggleAdmin() {
+    this.adminView = true;
+  }
+
+  toggleButtons() {
+    this.adminView = false;
   }
 
   // updatePints(pintsFromChild: Keg) {
